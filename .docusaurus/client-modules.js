bdf0fc9830d74b5251d22026e92c013bd55671ba
shrikantsuryawanshi39 <shrikantsuryawanshi155@gmail.com>
2025-11-27 20:19:55 +0530

    Deploy website - based on d53cc09d1c7ffbd6b52ab3be64c737e82255a8f9

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("D:\\Assignments\\dazzleduck-website\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("D:\\Assignments\\dazzleduck-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\Assignments\\dazzleduck-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\Assignments\\dazzleduck-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\Assignments\\dazzleduck-website\\src\\css\\custom.css"),
-];
